d62b9452ebf2136b52dab6d0f9831afc1ef852be
georgibaltiev <gbaltiev@gmail.com>
2025-02-22 08:59:26 +0200

    Add week 1 notes

---
+++ week_1/solutions/02-a-0500.sol
@@ -0,0 +1,12 @@
+# Създаваме наведнъж двете директории
+mkdir -p practice-test/test1
+
+# Преместваме се във вътрешната директория
+cd practice-test/test1
+
+# Създаваме нужния файл
+touch test.txt
+
+# Преместваме го извън test1
+
+mv test.txt ..
---
+++ week_1/solutions/02-a-0600.sol
@@ -0,0 +1,19 @@
+# Създаваме practice/01
+
+mkdir -p ~/practice/01
+
+# Създаваме файловете в нея
+
+touch ~/practice/01/f1
+touch ~/practice/01/f2
+touch ~/practice/01/f3
+
+# Създаваме си dir1 (в случай че я няма в home директорията ни)
+
+mkdir dir1
+
+# Копираме файловете
+
+cp ~/practice/01/f1 ~/dir1
+cp ~/practice/01/f2 ~/dir1
+cp ~/practice/01/f3 ~/dir1
